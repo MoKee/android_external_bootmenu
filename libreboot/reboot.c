70960b640bbb5fbe7e2c162b16f52111fe385991
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-04-27 22:36:21 +0200

    reboot applet cleanup
    Change-Id: I5d981c602004e72871762b59a254e1553c1e56f3

--- libreboot/reboot.c
+++ libreboot/reboot.c
@@ -4,8 +4,6 @@
 #include <sys/reboot.h>
 #include <unistd.h>
 
-//#include "cutils/android_reboot.h"
-
 #ifndef REBOOT_REASON_DEFAULT
 #define REBOOT_REASON_DEFAULT NULL
 #endif
@@ -17,7 +15,6 @@
 #define DBG_LEVEL 0
 //#define OVERRIDE_STOCK_BOOTLOADER
 
-//defined in system/core/libreboot
 #ifndef UNLOCKED_DEVICE
 #define OVERRIDE_STOCK_RECOVERY
 #endif
@@ -30,98 +27,95 @@
 
 int reboot_wrapper(const char* reason) {
 
-	int ret = 0;
-	int reboot_with_reason = 0;
-	int need_clear_reason = 0;
-	FILE * config;
+    int ret = 0;
+    int reboot_with_reason = 0;
+    int need_clear_reason = 0;
+    FILE * config;
 
-	if (reason == NULL || strlen(reason) == 0 ) {
+    if (reason == NULL || strlen(reason) == 0 ) {
 
-		reason = REBOOT_REASON_DEFAULT;
+        reason = REBOOT_REASON_DEFAULT;
 
-		system("rm -f '" BOARD_BOOTMODE_CONFIG_FILE "'");
-		pr_debug("rm -f " BOARD_BOOTMODE_CONFIG_FILE "\n");
+        system("rm -f '" BOARD_BOOTMODE_CONFIG_FILE "'");
+        pr_debug("rm -f " BOARD_BOOTMODE_CONFIG_FILE "\n");
 
-	} else {
+    } else {
 
-		// pass the reason to the kernel when we reboot
-		reboot_with_reason = 1;
+        // pass the reason to the kernel when we reboot
+        reboot_with_reason = 1;
 
-		system("mkdir -p /cache/recovery");
+        system("mkdir -p /cache/recovery");
 
-		config = fopen(BOARD_BOOTMODE_CONFIG_FILE,"w");
-		if (config == NULL) {
-			fprintf(stderr, "reboot: unable to create file " BOARD_BOOTMODE_CONFIG_FILE "\n");
-			reboot(RB_AUTOBOOT);
-			exit(1);
-		}
+        config = fopen(BOARD_BOOTMODE_CONFIG_FILE,"w");
+        if (config == NULL) {
+            fprintf(stderr, "reboot: unable to create file " BOARD_BOOTMODE_CONFIG_FILE "\n");
+            reboot(RB_AUTOBOOT);
+            exit(1);
+        }
 
-		// called for all reboot reasons
-		if ( 0 == strncmp(reason,"bootloader",10) ) {
+        // called for all reboot reasons
+        if ( 0 == strncmp(reason,"bootloader",10) ) {
 #ifdef OVERRIDE_STOCK_BOOTLOADER
-			// override bootloader reboot mode
-			ret = fputs("bootmenu", config);
-			pr_debug("%s->bootmenu " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
-			need_clear_reason = 1;
+            // override bootloader reboot mode
+            ret = fputs("bootmenu", config);
+            pr_debug("%s->bootmenu " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
+            need_clear_reason = 1;
 #endif
-		} else if ( 0 == strncmp(reason,"bootmenu", 8) ) {
+        } else if ( 0 == strncmp(reason,"bootmenu", 8) ) {
 
-			char ext_reason[128], dummy[10];
-			if (strlen(reason) > 8 && sscanf(reason, "%s %s", dummy, ext_reason) > 0) {
-				// allow "bootmenu recovery" or "bootmenu shell"
-				ret = fputs(ext_reason, config);
-				pr_debug("bootmenu %s " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", ext_reason, ret);
-			} else {
-				ret = fputs(reason, config);
-				pr_debug("%s->bootmenu " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
-			}
-			need_clear_reason = 1;
+            char ext_reason[128], dummy[10];
+            if (strlen(reason) > 8 && sscanf(reason, "%s %s", dummy, ext_reason) > 0) {
+                // allow "bootmenu recovery" or "bootmenu shell"
+                ret = fputs(ext_reason, config);
+                pr_debug("bootmenu %s " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", ext_reason, ret);
+            } else {
+                ret = fputs(reason, config);
+                pr_debug("%s->bootmenu " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
+            }
+            need_clear_reason = 1;
 
-		} else if ( 0 == strncmp(reason,"shell",5) ) {
+        } else if ( 0 == strncmp(reason,"shell",5) ) {
 
-			// override bootloader reboot mode
-			ret = fputs("shell", config);
-			pr_debug("%s->shell " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
-			need_clear_reason = 1;
+            // override bootloader reboot mode
+            ret = fputs("shell", config);
+            pr_debug("%s->shell " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
+            need_clear_reason = 1;
 
-		} else if ( 0 == strncmp(reason,"recovery",8) ) {
+        } else if ( 0 == strncmp(reason,"recovery",8) ) {
 #ifdef OVERRIDE_STOCK_RECOVERY
-			ret = fputs(reason, config);
-			pr_debug("%s->bootmenu recovery " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
-			need_clear_reason = 1;
+            ret = fputs(reason, config);
+            pr_debug("%s->bootmenu recovery " BOARD_BOOTMODE_CONFIG_FILE " (%d)\n", reason, ret);
+            need_clear_reason = 1;
 #else
-			system("rm -f '" BOARD_BOOTMODE_CONFIG_FILE "'");
-			pr_debug("allowing stock recovery reboot\n");
+            system("rm -f '" BOARD_BOOTMODE_CONFIG_FILE "'");
+            pr_debug("allowing stock recovery reboot\n");
 #endif
-		} else {
-			//hmm...
-			pr_debug("unhandled reason:%s\n", reason);
-			//need_clear_reason = 1;
-		}
+        } else {
+            pr_debug("unhandled reason:%s\n", reason);
+        }
 
-		fflush(config);
-		fclose(config);
+        fflush(config);
+        fclose(config);
+    }
 
-	}
+    // although we have a reason, ignore it on reboot
+    if (need_clear_reason) {
+        reboot_with_reason = 0;
+    }
 
-	// although we have a reason, ignore it on reboot
-	if (need_clear_reason) {
-		reboot_with_reason = 0;
-	}
+    system("sync");
 
-	system("sync");
+    #if (DBG_LEVEL)
+    system("sleep 3");
+    #endif
 
-	#if (DBG_LEVEL)
-	system("sleep 3");
-	#endif
+    if (reboot_with_reason)
+        ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, (char *)reason);
+    else
+        //android_reboot(ANDROID_RB_RESTART, 0, 0);
+        ret = reboot(RB_AUTOBOOT);
 
-	if (reboot_with_reason)
-		ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, (char *)reason);
-	else
-		//android_reboot(ANDROID_RB_RESTART, 0, 0);
-		ret = reboot(RB_AUTOBOOT);
-
-	return ret;
+    return ret;
 }
 
 //toolbox applet entry (ics)
@@ -133,10 +127,7 @@ int reboot_main(int argc, char *argv[])
 
     opterr = 0;
     do {
-        int c;
-
-        c = getopt(argc, argv, "np");
-
+        int c = getopt(argc, argv, "np");
         if (c == EOF) {
             break;
         }
@@ -175,4 +166,3 @@ int reboot_main(int argc, char *argv[])
     fprintf(stderr, "reboot returned\n");
     return 0;
 }
-
