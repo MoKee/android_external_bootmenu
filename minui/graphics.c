3801c639f1815fe3c82db8eed892fbf198d91900
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-21 06:22:02 +0200

    graphics: add support for "single buffering"
    When we don't have enough video memory for double buffering we
    fallback to "single buffering".
    
    Change-Id: I8bfab6d8cd6b54f0cc6c67edc41a4c37d8fbd4ba
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Signed-off-by: Tanguy Pruvot <tanguy.pruvot@gmail.com>

--- minui/graphics.c
+++ minui/graphics.c
@@ -59,6 +59,7 @@ static GGLContext *gr_context = 0;
 static GGLSurface gr_framebuffer[NUM_BUFFERS];
 static GGLSurface gr_mem_surface;
 static unsigned gr_active_fb = 0;
+static unsigned double_buffering = 0;
 
 static void * gr_fontmem = NULL;
 static void * gr_bigfontmem = NULL;
@@ -183,6 +184,12 @@ static int get_framebuffer(GGLSurface *fb)
 
     fb++;
 
+    /* check if we can use double buffering */
+    if (vi.yres * fi.line_length * NUM_BUFFERS > fi.smem_len)
+        return fd;
+
+    double_buffering = 1;
+
     fb->version = sizeof(*fb);
     fb->width = vi.xres;
     fb->height = vi.yres;
@@ -243,7 +250,7 @@ static void get_memory_surface(GGLSurface* ms) {
 
 static void set_active_framebuffer(unsigned n)
 {
-    if (n > 1) return;
+    if (n >= NUM_BUFFERS || !double_buffering) return;
     vi.yres_virtual = vi.yres * NUM_BUFFERS;
     vi.yoffset = n * vi.yres;
     vi.bits_per_pixel = PIXEL_SIZE * 8;
@@ -272,7 +279,8 @@ void gr_flip(void)
     GGLContext *gl = gr_context;
 
     /* swap front and back buffers */
-    gr_active_fb = (gr_active_fb + 1) & 1;
+    if (double_buffering)
+        gr_active_fb = (gr_active_fb + 1) % NUM_BUFFERS;
 
     /* copy data from the in-memory surface to the buffer we're about
      * to make active. */
